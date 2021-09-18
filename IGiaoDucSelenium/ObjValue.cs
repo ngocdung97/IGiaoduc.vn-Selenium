0001f0593de71e5aa4b7938f7f46bc7906fc3c43
ngocdung97 <buingocdung106@gmail.com>
2021-09-18 09:21:22 +0700

    first commit

---
+++ IGiaoDucSelenium/ObjValue.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+
+namespace IGiaoDucSelenium
+{
+    public class ObjValue
+    {
+        public string ID { get; set; }
+        public string Title { get; set; }
+        public List<Lesson> Lessons { get; set; }
+    }
+    public class Lesson
+    {
+        public string ID { get; set; }
+        public string TitleID { get; set; }
+        public string Content { get; set; }
+        public List<Exercise> Exercises { get; set; }
+    }
+
+    public class Exercise
+    {
+        public string ID { get; set; }
+        public string LessonID { get; set; }
+        public string Content { get; set; }
+        public string Link { get; set; }
+        public string Frame { get; set; }
+
+    }
+}
